5b419a3d3e283101cfa0167e9d4b38ff775c9a24
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-05-10 08:58:57 -0400

    this works

--- jsonmain.hs
+++ jsonmain.hs
@@ -1,12 +1,28 @@
-import Data.Aeson
-import Data.Aeson.TH
-import qualified Data.ByteString.Lazy.Char8 as BL
+{-# LANGUAGE DeriveGeneric #-}
 
-data Message = Message { foo :: String } deriving (Show, Eq)
+import Prelude hiding (putStr)
+
+import GHC.Generics
+
+import Data.Text (Text)
+import Data.Aeson (ToJSON(..), FromJSON(..), genericToEncoding, defaultOptions, encode)
+
+import Data.ByteString.Lazy (ByteString, putStr)
+
+data Person = Person {
+  name :: String
+  , age  :: Int
+  } deriving (Generic, Show)
+
+instance ToJSON Person where
+  -- No need to provide a toJSON implementation.
+
+  -- For efficiency, we write a simple toEncoding implementation, as
+  -- the default version uses toJSON.
+  toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON Person
+  -- No need to provide a parseJSON implementation.
 
 main :: IO ()
-main =
-  let doc = "{\"foo\": \"bar\"}"
-      obj = json doc :: Message
-  in
-    putStrLn obj
+main = putStr $ encode (Person {name = "Joe", age = 12})
