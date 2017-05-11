ecd8df06be557921cf9f3b398a3d39c7c4f29296
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-05-11 16:25:21 -0400

    some steps towards talking to subscription manager

--- s4wormhole_convergence/src/main.rs
+++ s4wormhole_convergence/src/main.rs
@@ -7,15 +7,22 @@ extern crate tokio_core;
 
 extern crate pretty_env_logger;
 
+use std::io;
+use std::io::Write;
 use std::env;
 use std::collections::HashMap;
 
+use futures::{Future, Stream};
+
 use std::time::Duration;
 
-use std::sync::mpsc::{Sender, Receiver, channel};
+use std::sync::mpsc::{SyncSender, Receiver, sync_channel};
 
 use std::thread::{JoinHandle, Builder, sleep};
 
+use tokio_core::reactor::Core;
+
+use hyper::Client;
 
 struct Options {
     subscription_manager_url: hyper::Uri,
@@ -30,7 +37,7 @@ fn parse_options(env: env::Args) -> Options {
         "http://localhost:8000/".parse::<hyper::Uri>().unwrap(),
 
         wormhole_rendezvous_url:
-        "ws://localhost:400/v1".parse::<hyper::Uri>().unwrap(),
+        "ws://localhost:4000/v1".parse::<hyper::Uri>().unwrap(),
 
     }
 }
@@ -42,22 +49,44 @@ struct SubscriptionClient {
 }
 
 
-fn subscription_worker(egress: Sender<String>) {
+fn subscription_worker(uri: hyper::Uri, egress: SyncSender<String>, delay: Receiver<()>) {
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+    let client = Client::new(&handle);
+
     loop {
+        let subscriptions = client.get(uri).and_then(|res| {
+            println!("Response: {}", res.status());
+            println!("Headers: \n{}", res.headers());
+            res.body().for_each(|chunk| {
+                io::stdout().write_all(&chunk).map_err(From::from)
+            });
+        });
         let _ = egress.send("hi".to_string()).unwrap();
-        // Give the server a little time to get going loooooolllll.
-        // this is how networking works.!!11!!
-        sleep(Duration::from_millis(10));
+        let _ = delay.recv();
     }
 }
 
 
-fn subscription_client(url: hyper::Uri) -> SubscriptionClient {
-    let _ = url;
-    let (sender, receiver) = channel();
+fn fixed_intervals(interval: Duration) -> Receiver<()> {
+    let (sender, receiver) = sync_channel(0);
+
+    Builder::new().name("timer".to_owned()).spawn(|| {
+        loop {
+            sleep(interval);
+            let _ = sender.send(()).unwrap();
+        }
+    });
+
+    return receiver;
+}
+
+
+fn subscription_client(uri: hyper::Uri) -> SubscriptionClient {
+    let (sender, receiver) = sync_channel(0);
     let b = Builder::new().name("subscriptions".to_owned());
     let t = b.spawn(|| {
-        subscription_worker(sender);
+        subscription_worker(uri, sender, fixed_intervals(Duration::from_secs(60)));
     }).unwrap();
     return SubscriptionClient {
         thread: t,
