360f1c59fc0ad75ca0854d4427fa2527f3e8fd24
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-05-12 11:14:54 -0400

    listNameplates works again

--- ListNameplates.hs
+++ ListNameplates.hs
@@ -1,4 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ListNameplates (listNameplates) where
+
+import qualified Data.List as List
+import qualified Data.Text.Lazy as Text
+import qualified Data.Text.Lazy.IO as TextIO
+import qualified Data.ByteString.Lazy as ByteString
 
 import Data.Aeson (encode, decode)
 
@@ -8,11 +15,41 @@ import qualified MagicWormholeModel as Model
 
 data State = Done | Starting | Bound
 
+expectAck conn = do
+  msg <- WebSockets.receiveData conn
+  case (msg) of
+    Model.Ack server_tx id   -> putStrLn "Received ack..."
+    Model.UndecodableMessage -> putStrLn "Received undecodable message!"
+    anything                 -> do
+      putStr "Received unexpected message: "
+      ByteString.putStr $ encode anything
+      ByteString.putStr "\n"
+
+
 listNameplates :: WebSockets.ClientApp ()
-listNameplates conn =
-  WebSockets.receiveData conn >>= (
-  \msg ->
-    case (msg) of
-      Model.UndecodableMessage -> putStrLn "boo"
-      Model.Welcome server_tx  -> putStrLn "welcome"
-  )
+listNameplates conn = do
+  msg <- WebSockets.receiveData conn
+  case (msg) of
+    Model.Welcome server_tx  -> putStrLn "Received welcome..."
+    Model.UndecodableMessage -> putStrLn "Received undecodable message!"
+    anything                 -> do
+      putStr "Received unexpected message: "
+      ByteString.putStr $ encode anything
+      ByteString.putStr "\n"
+
+  WebSockets.sendBinaryData conn $ Model.Bind "app" "server"
+  expectAck conn
+
+  WebSockets.sendBinaryData conn Model.List
+  expectAck conn
+
+  msg <- WebSockets.receiveData conn
+  case (msg) of
+    Model.Nameplates nameplates   -> do
+      putStr "Nameplates: "
+      TextIO.putStrLn $ Text.concat $ List.intersperse " " nameplates
+    Model.UndecodableMessage -> putStrLn "Received undecodable message!"
+    anything                 -> do
+      putStr "Received unexpected message: "
+      ByteString.putStr $ encode anything
+      ByteString.putStr "\n"
