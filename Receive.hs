e32487a8040ead882823c01784b6bd573bd0251b
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-05-15 20:14:01 -0400

    not that this works either

--- Receive.hs
+++ Receive.hs
@@ -8,12 +8,14 @@ import Data.Text.Lazy (Text, concat, pack)
 import Data.Text.Lazy.IO (putStrLn)
 import Data.Text.Lazy.Encoding (encodeUtf8, decodeUtf8)
 
-import Data.ByteString.Lazy (ByteString, toStrict)
+import Data.ByteString.Lazy (ByteString, fromStrict, toStrict)
+import qualified Data.ByteString.Lazy as B
 
 import Data.Hex (hex)
 
-import Crypto.Hash.SHA256 (init, update, finalize)
-import Crypto.HKDF (hkdfExpand)
+import Crypto.Hash.SHA256 (init, hashlazy)
+
+import Crypto.KDF.HKDF (expand, extract)
 
 import Crypto.Saltine.Class (decode)
 import Crypto.Saltine.Core.SecretBox (Key, Nonce, secretbox, newNonce)
@@ -28,34 +30,28 @@ import qualified MagicWormholeModel as Model
 import ListNameplates (WormholeError(..), expectAck)
 import OpenMailbox (openMailbox)
 
-sha256 :: Text -> ByteString
-sha256 plaintext =
-  digest
-  where
-    digest = finalize ctx
-    ctx = update ctx0 $ encodeUtf8 plaintext
-    ctx0 = init
+sha256 :: ByteString -> ByteString
+sha256 = fromStrict . hashlazy
 
-wormhole_purpose :: Text -> Text -> ByteString
+wormhole_purpose :: ByteString -> ByteString -> ByteString
 wormhole_purpose side phase =
-  concat ["wormhole:phase:", sha256 side, sha256 phase]
+  B.concat ["wormhole:phase:" :: ByteString, sha256 side, sha256 phase]
 
 hkdf :: ByteString -> Int -> ByteString -> ByteString
 hkdf skm outlen ctxinfo =
-  hkpfExpand SHA256 skm ctxinfo outlen
+  expand (extract "" skm) ctxinfo outlen
 
 derive_key :: ByteString -> ByteString -> ByteString
 derive_key key purpose =
   hkdf key 32 purpose
 
-derive_phase_key :: ByteString -> ByteString -> ByteString
+derive_phase_key :: ByteString -> ByteString -> ByteString -> ByteString
 derive_phase_key key side phase =
-  derive_key key $ wormhole_purpose side phase
+  derive_key key (wormhole_purpose side phase)
 
-encrypt_data :: Key -> ByteString -> IO ByteString
-encrypt_data key plaintext = do
-  nonce <- newNonce
-  secretbox key nonce (toStrict plaintext)
+encrypt_data :: Key -> Nonce -> ByteString -> ByteString
+encrypt_data key nonce plaintext =
+  fromStrict $ secretbox key nonce (toStrict plaintext)
 
 receive :: Text -> Text -> Text -> Connection -> IO (Either WormholeError ())
 receive appid side code conn = do
@@ -72,14 +68,14 @@ receive appid side code conn = do
           let
             plaintext = ("{}" :: Text)
             phase = ("version" :: Text)
-            data_key = derive_phase_key (finish body side) (encodeUtf8 phase)
           in
             do
-              key_obj <- decode (toStrict data_key)
-              case key_obj of
+              let data_key = derive_phase_key (encodeUtf8 code) (finish body side) (encodeUtf8 phase)
+              case decode (toStrict data_key) of
                 Nothing -> pure $ Left CryptoFailure
                 Just k  -> do
-                  ciphertext <- encrypt_data k (encodeUtf8 plaintext)
+                  nonce <- newNonce
+                  let ciphertext = encrypt_data k nonce (encodeUtf8 plaintext)
                   _ <- sendBinaryData conn $ Model.Add "version" (decodeUtf8 $ hex ciphertext)
                   pure $ Right ()
 
